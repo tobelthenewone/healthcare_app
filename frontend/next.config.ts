87dd52ecf78da4f9f6ae716ffd637cdfc7db7f0e
tobelthenewone <tobelmikiyas@gmail.com>
2026-07-15 20:21:48 +0300

    allowed one more origin for testing purposes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ['10.19.134.40'],
 };
 
 export default nextConfig;
