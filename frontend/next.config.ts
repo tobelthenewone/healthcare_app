b755aca4ab62e9394c592bf23dee355547ca5bf9
tobelthenewone <tobelmikiyas@gmail.com>
2026-07-27 14:58:24 +0300

    fix: update allowedDevOrigins to correct IP address

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ['10.19.134.40'],
+  allowedDevOrigins: ['10.201.99.40'],
 };
 
 export default nextConfig;
